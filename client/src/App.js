906d3b02cad7f9b56c9be61da1b9a40b533f75dc
delfinadelossantos <delfinadelossantos@gmail.com>
2023-07-07 08:07:43 -0300

    started frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
-import './App.css';
+import "./App.css";
+import { Route, useLocation } from "react-router-dom";
+import { Home, Detail, Form, Landing } from "./views/index";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
+  //useLocation es un hook que permite acceder a la ubicación actual (path)
+  const location = useLocation();
+
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      {/* Lo utilizo para que el navbar sólo se muestre en los paths requeridos. */}
+      {location.pathname !== "/" && <NavBar />}
+
+      <Route exact path="/">
+        <Landing />
+      </Route>
+
+      <Route path="/home">
+        <Home />
+      </Route>
+
+      <Route path="/detail">
+        <Detail />
+      </Route>
+
+      <Route path="/form">
+        <Form />
+      </Route>
     </div>
   );
 }
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "./navbar.css";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div className="link-container">
+        <Link to="/home">Home</Link>
+        <Link to="/form">Form</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
