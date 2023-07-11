042b13cb1c73840b5e2d7697cac3ba64eede268c
delfinadelossantos <delfinadelossantos@gmail.com>
2023-07-11 12:31:35 -0300

    created getRecipes

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import "./home.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRecipes } from "../../redux/actions";
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRecipes());
+  }, []);
+
+  //useSelector permite acceder al estado global sin necesidad de recibir props. Se suscribe al estado.
+  const recipes = useSelector((state) => state.recipes);
+
   return (
     <div className="home-cont">
       <h1>Home</h1>
       <div>
-        <CardsContainer />
+        <CardsContainer recipes={recipes}></CardsContainer>
       </div>
     </div>
   );
