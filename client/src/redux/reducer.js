04c2cdcb104723d0b4a476f0dfafd92213d189da
delfinadelossantos <delfinadelossantos@gmail.com>
2023-07-13 15:05:00 -0300

    fixed pagination

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,10 +1,17 @@
 //El reducer es una función que recibe el estado a modificar y la action que indica qué tiene que hacer.
-import { GET_RECIPES, PAGINATION } from "./actions";
+import {
+  FILTER_BY_ORIGIN,
+  GET_DIETS,
+  GET_RECIPES,
+  PAGINATION,
+} from "./actions";
 
 const initialState = {
   recipes: [],
   pagination: [],
   currentPage: 0,
+  filtered: [],
+  diets: [],
 };
 
 //Cuando la aplicación recién inicia, el estado es initialState.
@@ -17,7 +24,7 @@ const rootReducer = (state = initialState, action) => {
       return {
         ...state,
         recipes: action.payload,
-        pagination: action.payload.splice(0, itemsPerPage),
+        pagination: [...action.payload].splice(0, itemsPerPage),
       };
     case PAGINATION:
       const nextPage = state.currentPage + 1;
@@ -38,6 +45,29 @@ const rootReducer = (state = initialState, action) => {
         pagination: [...state.recipes].splice(firstIndex, itemsPerPage),
         currentPage: action.payload === "next" ? nextPage : prevPage,
       };
+    case GET_DIETS:
+      return {
+        ...state,
+        diets: action.payload,
+      };
+    case FILTER_BY_ORIGIN:
+      if (action.payload === "api") {
+        return {
+          ...state,
+          filtered: [...state.recipes].filter(
+            (recipe) => recipe.createdInDb === false
+          ),
+        };
+      } else if (action.payload === "db") {
+        return {
+          ...state,
+          filtered: [...state.recipes].filter(
+            (recipe) => recipe.createdInDb === true
+          ),
+        };
+      } else {
+        return { ...state };
+      }
 
     default:
       return { ...state };
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -3,17 +3,32 @@ import CardsContainer from "../../components/CardsContainer/CardsContainer";
 import "./home.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getRecipes, pagination } from "../../redux/actions";
+import {
+  filterByOrigin,
+  getDiets,
+  getRecipes,
+  pagination,
+} from "../../redux/actions";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getRecipes());
+    dispatch(getDiets());
   }, []);
+
+  const filterRecipesByOrigin = (event) => {
+    const origin = event.target.value;
+    dispatch(filterByOrigin(origin));
+  };
 
   //useSelector permite acceder al estado global sin necesidad de recibir props. Se suscribe al estado.
   const recipes = useSelector((state) => state.recipes);
+
+  const diets = useSelector((state) => state.diets);
+
+  const handleDiets = (event) => {};
 
   //Paginado global
   const recipesPagination = useSelector((state) => state.pagination);
@@ -32,7 +47,36 @@ const Home = () => {
       <div className="home-pagination">
         <button onClick={prevPage}>Prev</button>
         <button onClick={nextPage}>Next</button>
+
+        <label>Filter by Origin: </label>
+        <select onChange={filterRecipesByOrigin}>
+          <option value="All">All</option>
+          <option value="api">API</option>
+          <option value="db">Database</option>
+        </select>
+
+        <label>Filter by Diet: </label>
+        <select value={diets} onChange={handleDiets} name="diets">
+          {diets.map((diet) => (
+            <option key={diet.id} value={diet.id}>
+              {diet.name}
+            </option>
+          ))}
+        </select>
+
+        <label>Sort recipes: </label>
+        <select>
+          <option value="A-Z">From A to Z</option>
+          <option value="Z-A">From Z to A</option>
+        </select>
+
+        <label>Sort by HealthScore: </label>
+        <select>
+          <option value="">Highest HealthScore</option>
+          <option value="">Lowest HealthScore</option>
+        </select>
       </div>
+
       <div>
         <CardsContainer recipes={recipesPagination}></CardsContainer>
       </div>
